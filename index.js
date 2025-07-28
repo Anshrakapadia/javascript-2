3babad1a28951d7dd8fcc48ac55feca36ca591c0
Anshrakapadia <anshrakapadia@gmail.com>
2025-07-28 11:19:19 -0700

    javascript-2

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("forwardCard").addEventListener("click",()=>{
+let result= "";
+for(let i =1; i <=20 ; i++){
+    result += i + "<br>";
+}document.getElementById("output").innerHTML = "<h2>forward Counting</h2> + result";});
+document.getElementById("Backward card")addEventListener("click",()=>{ let result ="";
+for(let i= 20 ; i >= 1; i--){
+    result += i + "<br>";
+}document.getElementById("tableCard").addEventListener("click"))
+// var num = prompt("enter any number for Tabel");
+// document.writeln("<h3>Multiplication Table" + " of " + num + "</h3>");
+// for (var i = 1; i <= 10; i++) {
+//     document.writeln(num + "x" + i + "=" + (num * i) + "<br>")
+// }
